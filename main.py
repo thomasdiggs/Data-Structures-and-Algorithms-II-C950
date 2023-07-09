bc62dedcd628bf7bf8637d29fe7c410ce5c23c00
Thomas Diggs <thomasdiggs@gmail.com>
2023-07-09 12:57:13 -0600

    update to import functions

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# Thomas Diggs 010815435
+# Name: Thomas Diggs
+# Student ID: 010815435
 
-from hashtable import *
-from import_csv import *
-from menu import *
+from modules.hash_table import *
+from modules.import_csv import *
+from modules.menu import *
 
 
 class Main:
-    # Instantiate the class ChainingHashTable
+    # instantiate the class ChainingHashTable
     hash_table = ChainingHashTable()
 
-    # Call import_packages from the import-csv.py module
-    import_packages('packages.csv', hash_table)
+    # begin calling import functions from the import-csv.py module
+    # import package objects into the newly created hash_table
+    import_packages("csv_files/packages.csv", hash_table)
+
+    # import addresses into a one-dimensional list
+    address_data = import_addresses("csv_files/addresses.csv")
+    print(address_data)
+
+    # import distances to all addresses into a two-dimensional list
+    # distance_data[2][6] is the same as distance_data[6][2]
+    distance_data = import_distances("csv_files/distances.csv")
+    print(distance_data)
 
     # Start of program's user interface
     start_ui(hash_table)
---
+++ modules/import_csv.py
@@ -0,0 +1,49 @@
+import csv
+from modules.packages import *
+
+
+# parse the packages csv file
+def import_packages(filename, hash_table):
+    with open(filename) as csv_file:
+        reader = csv.reader(csv_file, delimiter=",")
+        # loop through each row in the csv file
+        # O(n)
+        for row in reader:
+            # storing values into variables to pass into insert function and package object
+            pid = int(row[0])
+            address = row[1]
+            city = row[2]
+            state = row[3]
+            zip_code = int(row[4])
+            deadline = row[5]
+            weight = int(row[6])
+            # insert package id as an integer for the key and a package object as the value
+            hash_table.insert(pid, Package(pid, address, city, state, zip_code, deadline, weight))
+
+
+# parse the distance csv file
+def import_distances(filename):
+    with open(filename) as csv_file:
+        reader = csv.reader(csv_file, delimiter=",")
+        # store each row from csv file into distance_data list
+        distance_data = list(reader)
+        # iterate over the rows and columns to fill in the missing values from the csv
+        # this creates a complete distance table
+        # O(n^2)
+        for i in range(len(distance_data)):
+            for j in range(len(distance_data)):
+                distance_data[i][j] = distance_data[j][i]
+        return distance_data
+
+
+# parse the address csv file
+def import_addresses(filename):
+    with open(filename) as csv_file:
+        reader = csv.reader(csv_file, delimiter=",")
+        address_data = []
+        # loop through each row in the csv file
+        # O(n)
+        for row in reader:
+            # append only the address (element 3, index 2) to the list
+            address_data.append(row[2])
+        return address_data
